29fe80d5c1337f1c7ed04304575c17fe4689c825
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-11 19:01:04 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid 
+  include Dance #bundling and including the Dance class, allowing the Kid class to use all the Dance module's methods as instance methods.  
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+end 
+
