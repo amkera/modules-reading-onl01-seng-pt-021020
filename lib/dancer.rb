ac770be62a28f1a5347f249838251e691bdb3ef0
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-11 19:11:21 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  extend MetaDancing
+ 
 
   include Dance 
   
@@ -10,6 +10,8 @@ class Dancer
   def initialize(name)
     @name = name 
   end 
+  
+  extend MetaDancing
   
   
 end 
