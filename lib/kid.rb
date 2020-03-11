965de0b05f8d6d46daccfc9c53213cc19db4fbbd
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-11 19:05:13 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,8 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
+  extend MetaDancing
+  
   include Dance #bundling and including the Dance class, allowing the Kid class to use all the Dance module's methods as instance methods.  
   
   attr_accessor :name 
